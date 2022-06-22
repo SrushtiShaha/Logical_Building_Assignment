3265238381304b3ab0ee7a5a5605826206f690be
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2022-06-23 00:28:39 +0530

    Add files via upload

---
+++ Assignment 01/LB.1(2) Program To Print Five Times "Marvellous" On Screen.c
@@ -0,0 +1,22 @@
+// Logic Building Assignment 1 Q.2
+
+#include <stdio.h>
+#include <conio.h>
+
+void Display()
+{
+          int i = 0;
+          
+          for( i = 1; i <= 5; i++ )
+          {
+                  printf("\n Marvellous \n");
+          }
+}
+
+int main()
+{
+          Display();
+          
+          getch();
+          return 0;
+}
