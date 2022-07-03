4967291e637799bd82db5f06bb7fc1a05dbf6873
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2022-07-03 23:50:30 +0530

    Add files via upload

---
+++ Assignment 01/3.1 Program To Print 5 To 1 Numbers On Screen.C
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+
+void Display()
+{
+                int i = 0;
+
+                i = 5;
+
+                while( i > 0 )
+                {
+                                printf("\n %d \n",i);
+                                i--;
+                }
+}
+
+int main()
+{
+                Display();
+
+                return 0;
+}
