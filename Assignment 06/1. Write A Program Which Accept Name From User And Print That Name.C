6bd11789dc1baa984bbcbe83d31417a361d6610f
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2022-07-07 22:55:20 +0530

    Add files via upload

---
+++ Assignment 06/1. Write A Program Which Accept Name From User And Print That Name.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+                char Name[30];
+
+                printf("\n Please Enter Full Name => ");
+                gets(Name);                             ///scanf("%s",&Name);
+
+                printf("\n Your Name Is => %s",Name);
+
+                return 0;
+}
