615b80307680f5634c22d76b67b054c1ec859b68
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2022-07-08 18:43:32 +0530

    Add files via upload

---
+++ Assignment 07/3. Write A Program Which Accept Number From User And Print It's Number line.C
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+
+void Display( int iNo )
+{
+                int i = 0;
+
+                for( i = -iNo; i <= iNo; i++ )
+                {
+                                printf("\t %d ",i);
+                }
+}
+
+int main()
+{
+                int iValue = 0;
+
+                printf("\n Enter Number => ");
+                scanf("%d",&iValue);
+
+                Display( iValue );
+
+                return 0;
+}
