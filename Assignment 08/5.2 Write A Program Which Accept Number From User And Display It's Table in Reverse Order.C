63edb157370cf4950614c8369368eef575faad1f
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2022-07-12 23:40:21 +0530

    Add files via upload

---
+++ Assignment 08/5.2 Write A Program Which Accept Number From User And Display It's Table in Reverse Order.C
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+
+void TableRev( int iNo )
+{
+                int i = 0, Temp = 0;
+
+                Temp = iNo * 10;
+
+                for( i = 10; i > 0; i-- )
+                {
+                                printf("%d \t",Temp);
+
+                                Temp = Temp - iNo;
+                }
+}
+
+int main()
+{
+                int iValue = 0;
+
+                printf("\n Enter A Number => ");
+                scanf("%d",&iValue);
+
+                TableRev( iValue );
+
+                return 0;
+}
