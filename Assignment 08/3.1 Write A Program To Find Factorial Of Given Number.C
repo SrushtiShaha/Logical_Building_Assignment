e40c45ccdfa1a82693d067616a8eded6f61775eb
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2022-07-12 23:39:07 +0530

    Add files via upload

---
+++ Assignment 08/3.1 Write A Program To Find Factorial Of Given Number.C
@@ -0,0 +1,47 @@
+/// If The User Gives Negative Odd Number Then Answer Will Be Negative Else If User Gives Negative Even Number Then Answer Will Be Positive...
+
+#include<stdio.h>
+#include<conio.h>
+
+int Factorial( int iNo )
+{
+                int i = 1, Temp = 1;
+
+                i = 1;
+
+                if( iNo < 0 )
+                {
+                                iNo = 0 - iNo;
+                }
+
+                while( iNo >= i )
+                {
+                                Temp = Temp * i;
+                                i++;
+                }
+
+                i = -1;
+
+                while( iNo <= i )
+                {
+                                Temp = Temp * i;
+                                i--;
+                }
+
+                return Temp;
+}
+
+int main()
+{
+                int iValue = 0, iRet = 0;
+
+                printf("\n Enter A Number => ");
+                scanf("%d",&iValue);
+
+                iRet = Factorial( iValue );
+
+                printf("\n Factorial Of %d Is %d. \n",iValue,iRet);
+
+                return 0;
+}
+
