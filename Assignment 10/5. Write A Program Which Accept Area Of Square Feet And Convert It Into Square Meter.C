5421c49a57000c51e9b47e0540916a82ef368aea
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2022-07-18 18:51:03 +0530

    Add files via upload

---
+++ Assignment 10/5. Write A Program Which Accept Area Of Square Feet And Convert It Into Square Meter.C
@@ -0,0 +1,26 @@
+/// 1 Square Feet = 0.0929 Square Meter
+
+#include<stdio.h>
+#include<conio.h>
+
+double SquareMeter( int iValue )
+{
+                double dAns = 0.0;
+
+                dAns = iValue * 0.0929;
+
+                return dAns;
+}
+
+int main()
+{
+                int iValue = 0;
+                double dRet = 0.0;
+
+                printf("\n Enter Area Of Square => ");
+                scanf("%d",&iValue);
+
+                dRet = SquareMeter( iValue );
+
+                printf("\n Square Meter => %lf \n",dRet);
+}
