967556c2f7379412f784b7bc02b3b0c308333f0d
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2022-07-29 00:06:14 +0530

    Update Write A Program  Which Accept Number From User And Return The Count Of Even Digits.c

--- Assignment 13/Write A Program  Which Accept Number From User And Return The Count Of Even Digits.c
+++ Assignment 13/Write A Program  Which Accept Number From User And Return The Count Of Even Digits.c
@@ -1,9 +1,36 @@
 #include <stdio.h>
 #include <conio.h>
 
+int CountEven( int iNo )
+{
+            int iRes = 0, iCnt = 0;
+            
+            while( iNo > 0 )
+            {
+                        iRes = iNo % 10;
+                        
+                        if( iRes % 2 == 0 )
+                        {
+                                    iCnt++;
+                        }
+                        
+                        iNo = iNo / 10;
+            }
+            
+            return iCnt;
+}
+
 int main()
 {
-  
-  printf("Hello, World!");
-  return 0;
+            int iValue = 0, iRet = 0;
+            
+            printf("\n Enter A Number => ");
+            scanf("%d",&iValue);
+            
+            iRet = CountEven( iValue );
+            
+            printf("\n %d \n",iRet);
+            
+            getch();
+            return 0;
 }
