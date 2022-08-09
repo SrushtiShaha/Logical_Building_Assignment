f5d95b34805dc241658d3fa33c5e1c86556e9f47
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2022-08-09 23:51:11 +0530

    Update 2. Accept Number From User And Display The Pattern.c

---
+++ Assignment 14/2. Accept Number From User And Display The Pattern.c
@@ -0,0 +1,30 @@
+/// Input  : 5
+/// Output : 5 # 4 # 3 # 2 # 1 #
+
+#include <stdio.h>
+#include <conio.h>
+
+void Pattern( iNo )
+{           
+            while( iNo > 0 )
+            {
+                        printf(" %d \t # \t",iNo);
+                        
+                        iNo--;
+            }
+            
+            return;
+}
+
+int main()
+{
+            int iValue = 0;
+            
+            printf("\n Enter Number Of Elements => ");
+            scanf("%d",&iValue);
+            
+            Pattern( iValue);
+            
+            getch();
+            return 0;
+}
