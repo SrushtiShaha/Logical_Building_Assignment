3d8a9764671a1af83362be60fe8ac6221f855fd7
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2022-12-04 23:25:29 +0530

    Logical_Building_Assignment

---
+++ Assignment 10/04_Write_A_Program_Which_Accept_Temperature_In_Fahrenheit_And_Convert_It_Into_Celsius.c
@@ -0,0 +1,35 @@
+/// Write A Program Which Accept Temperature In Fahrenheit And Convert It Into Celsius. (1 celsius = (Fahrenheit - 32) * (5/9) ) 
+/// Input : 10 
+/// Output : -12.2222 (10 - 32) * (5/9)
+/// Input : 34 
+/// Output : 1.11111 (34 - 32) * (5/9)
+
+//Compiler version gcc  6.3.0
+
+#include<stdio.h> 
+#include<conio.h>
+
+double FhtoCs(float fTemp) 
+{ 
+      double Celcius = 0.0;
+      
+      Celcius = ( ( fTemp - 32 ) * ( 0.5555555 ) );  /// ( 5 / 9 ) = 0.5555555
+      
+      return Celcius;
+} 
+
+int main() 
+{ 
+     float fValue = 0.0; 
+     double dRet = 0.0; 
+     
+     printf("\n Enter Temperature In Fahrenheit -> "); 
+     scanf("%f",&fValue); 
+     
+     dRet = FhtoCs(fValue); 
+     
+     printf("\n After Converting Temperature To Celcius Ans Is => %0.5lf \n",dRet); 
+     
+     getch();
+     return 0; 
+}
