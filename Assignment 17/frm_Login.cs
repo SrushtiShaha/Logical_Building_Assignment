47da0891701acffebf03bcb9b9e77279846c7011
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2023-06-16 02:36:18 -0700

    Logical_Building_Assignment

---
+++ Assignment 17/frm_Login.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Student_Management_System
+{
+    public partial class frm_Login : Form
+    {
+        public frm_Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Only_Text(object sender, KeyPressEventArgs e)
+        {
+            if(!(char.IsLetter(e.KeyChar) || (e.KeyChar == (char)Keys.Back) || (e.KeyChar == (char)Keys.Space)))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void frm_Login_Load(object sender, EventArgs e)
+        {
+            lbl_Note.Text = "Enter Correct Username And Password";
+        }
+
+        private void btn_Submit_Click(object sender, EventArgs e)
+        {
+            if(tb_Username.Text == "s" && tb_Password.Text == "s123")
+            {
+                MessageBox.Show("Login Successful!!!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                frm_Add_New_Student obj = new frm_Add_New_Student();
+                obj.Show();
+                this.Hide();
+            }
+            else
+            {
+                lbl_Note.Text = "Invalid Username Or Password";
+                lbl_Note.ForeColor = Color.Red;
+
+                tb_Username.Clear();
+                tb_Password.Clear();
+            }    
+        }
+    }
+}
