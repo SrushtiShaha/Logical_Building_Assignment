3d509c6863b95863ce721505fe488dd912ca9128
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2023-06-27 02:26:02 +0800

    Logical_Building_Assignment

---
+++ Assignment 17/04_1_Accept_Number_Of_Rows_And_Number_Of_Columns_From_User_And_Display_Below_Pattern.c
@@ -0,0 +1,59 @@
+// 04_Accept_Number_Of_Rows_And_Number_Of_Columns_From_User_And_Display_Below_Pattern.
+// Input :  iRow = 5 iCol = 5
+// Output :  1  2  3  4  5
+//          -1 -2 -3 -4 -5
+//           1  2  3  4  5
+//          -1 -2 -3 -4 -5
+//           1  2  3  4  5
+
+#include<stdio.h>
+#include<conio.h>
+
+void Pattern(int iRow, int iCol)
+{
+        int i = 0, j = 0, iR = 1, iC = -1;
+
+        for( i = 1; i <= iRow; i++ )
+        {
+                for( j = 1; j <= iCol; j++ )
+                {
+                        if( i % 2 != 0 )
+                        {
+                                printf(" %3d", iR);
+
+                                iR++;
+
+                                if( iR > 5 )
+                                {
+                                        iR = 1;
+                                }
+                        }
+                        else if( i % 2 == 0 )
+                        {
+                                printf("  %2d", iC);
+
+                                iC--;
+
+                                if( iC < -5 )
+                                {
+                                        iC = -1;
+                                }
+                        }
+                }
+
+                printf("\n");
+        }
+}
+
+int main()
+{
+        int iValue1 = 0, iValue2 = 0;
+
+        printf("\n Enter Number Of Rows And Columns -> ");
+        scanf("%d%d",&iValue1,&iValue2);
+
+        Pattern( iValue1, iValue2 );
+
+        getch();
+        return 0;
+}
